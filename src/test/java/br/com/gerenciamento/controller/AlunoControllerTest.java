81552407bb37779cb5d9b3e540c9b073af2961b3
ThaysonScript <thaysonguedes01@gmail.com>
2026-07-20 21:09:15 -0300

    testes faltantes em aluno controller

--- src/test/java/br/com/gerenciamento/controller/AlunoControllerTest.java
+++ src/test/java/br/com/gerenciamento/controller/AlunoControllerTest.java
@@ -1,10 +1,54 @@
 package br.com.gerenciamento.controller;
 
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class AlunoControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test    
+    public void pesquisarAlunoNomeVazioTest() throws Exception {
+        mockMvc.perform(post("/pesquisar-aluno")
+                .param("nome", ""))
+                .andExpect(status().isOk())
+                .andExpect(view().name("Aluno/pesquisa-resultado"))
+                .andExpect(model().attributeExists("ListaDeAlunos"));
+    }
+
+    @Test
+    public void acessarFormularioInserirAlunosTest() throws Exception {
+        mockMvc.perform(get("/inserirAlunos"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("Aluno/formAluno"))
+                .andExpect(model().attributeExists("aluno"));
+    }
+
+    @Test
+    public void listagemAlunosAdicionadosTest() throws Exception {
+        mockMvc.perform(get("/alunos-adicionados"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("Aluno/listAlunos"))
+                .andExpect(model().attributeExists("alunosList"));
+    }
+
+    @Test
+    public void filtroAlunosPageTest() throws Exception {
+        mockMvc.perform(get("/filtro-alunos"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("Aluno/filtroAlunos"));
+    }
 }
